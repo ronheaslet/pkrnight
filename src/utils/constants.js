fc2265dcc9ecf968bb3c6923a056096c860fd1eb
ronheaslet <ron@ronheaslet.com>
2026-01-01 00:54:59 -0500

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,21 @@
+/**
+ * Shared constants for PKR Night
+ */
+
+export const FALLBACK_BLINDS = [
+  { level: 1, sb: 25, bb: 50, ante: 0, duration: 15 },
+  { level: 2, sb: 50, bb: 100, ante: 0, duration: 15 },
+  { level: 3, sb: 75, bb: 150, ante: 0, duration: 15 },
+  { level: 4, sb: 100, bb: 200, ante: 25, duration: 15 },
+  { level: 5, sb: 150, bb: 300, ante: 25, duration: 15 },
+  { level: 6, sb: 200, bb: 400, ante: 50, duration: 15 },
+  { level: 7, sb: 300, bb: 600, ante: 75, duration: 15 },
+  { level: 8, sb: 400, bb: 800, ante: 100, duration: 15 },
+  { level: 9, sb: 500, bb: 1000, ante: 100, duration: 15 },
+  { level: 10, sb: 600, bb: 1200, ante: 200, duration: 15 },
+  { level: 11, sb: 800, bb: 1600, ante: 200, duration: 12 },
+  { level: 12, sb: 1000, bb: 2000, ante: 300, duration: 12 },
+  { level: 13, sb: 1500, bb: 3000, ante: 400, duration: 12 },
+  { level: 14, sb: 2000, bb: 4000, ante: 500, duration: 12 },
+  { level: 15, sb: 3000, bb: 6000, ante: 1000, duration: 10 },
+]
